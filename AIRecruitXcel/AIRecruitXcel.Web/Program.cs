cacc9237e95e246f7089dd440d0babf447c86c33
HungLe <hle@jobadder.com>
2024-04-16 16:56:14 +0700

    resolve conflict

--- AIRecruitXcel/AIRecruitXcel.Web/Program.cs
+++ AIRecruitXcel/AIRecruitXcel.Web/Program.cs
@@ -1,36 +1,41 @@
+using AIRecruitXcel.Core;
+
 namespace AIRecruitXcel.Web
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      var builder = WebApplication.CreateBuilder(args);
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-      // Add services to the container.
-      builder.Services.AddControllersWithViews();
+            // Add services to the container.
+            string model = "gpt-3.5-turbo";
+            string key = "key_placeholder";
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IInterviewSupporter>(x => new InterviewSupporter(model, key));
 
-      var app = builder.Build();
+            var app = builder.Build();
 
-      // Configure the HTTP request pipeline.
-      if (!app.Environment.IsDevelopment())
-      {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-      app.UseRouting();
+            app.UseRouting();
 
-      app.UseAuthorization();
+            app.UseAuthorization();
 
-      app.MapControllerRoute(
-          name: "default",
-          pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
-      app.Run();
+            app.Run();
+        }
     }
-  }
 }
